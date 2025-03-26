b061c94567a3d52371572550533627be056181ab
Al Ameen Khader <alameenkhader@gmail.com>
2025-03-26 15:37:23 -0700

    check

--- downloader.rb
+++ downloader.rb
@@ -1,7 +1,31 @@
 require_relative 'config'
+require 'pathname'
+
+def get_available_space(path)
+  df_output = `df -k "#{path}" 2>/dev/null`
+  return 0 unless $?.success?
+
+  available_kb = df_output.split("\n")[1].split(/\s+/)[3].to_i
+  available_kb.to_f / 1024  # Convert to MB
+end
 
 def download_movies(list)
   FileUtils.mkdir_p(DOWNLOAD_PATH)
+
+  # Clean up partial downloads
+  Dir.glob(File.join(DOWNLOAD_PATH, "*.part")).each do |part_file|
+    LOGGER.info "Cleaning up partial download: #{part_file}"
+    File.delete(part_file)
+  end
+
+  # Check available disk space
+  available_mb = get_available_space(DOWNLOAD_PATH)
+
+  if available_mb < 500
+    LOGGER.error "Insufficient disk space. Only #{available_mb.round(2)}MB available."
+    return
+  end
+
   list.each do |item|
     url = item[:einthusan_url]
     title = item[:title]
--- extractor.rb
+++ extractor.rb
@@ -22,9 +22,7 @@ def extract_movie_list(url)
     title = block2.at_css('.title h3')&.text || "Unknown Title"
     image_url = movie.at_css('.block1 img')&.[]('src') || "No Image URL"
     relative_url = block2.at_css('.title')&.[]('href') || "No URL"
-    # Fix: Look for time element in block3
     release_date = movie.at_css('.block3 .stats time')&.[]('datetime')
-    p "Found release date: #{release_date} for movie: #{title}" # Debug line
     release_datetime = release_date ? DateTime.parse(release_date) : DateTime.new(1970)
     full_url = BASE_URL + relative_url
     file_name = "#{title.downcase.gsub(/\s+/, '_')}.mp4"
