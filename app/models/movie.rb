58a12c8edf8d6db67ab970e1d99b4aca5b7b8291
Al Ameen Khader <alameenkhader@gmail.com>
2025-09-23 10:56:37 -0700

    gd

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,12 @@ class Movie < ApplicationRecord
   validates :einthusan_url, presence: true, uniqueness: true
 
   scope :recent, -> { order(released_at: :desc) }
+
+  has_one_attached :video
+  # @message.images.attach(
+  #   io: File.open("/path/to/file"),
+  #   filename: "file.pdf",
+  #   content_type: "application/pdf",
+  #   identify: false
+  # )
 end
